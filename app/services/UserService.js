e2056ed2d1c291668325220f67b2c34138b25c0e
Joel Hoil <yoelvann@gmail.com>
2022-04-25 15:10:53 -0500

    sprint 2 - req. 2 implemented

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -2,7 +2,11 @@ const User = require('./../../app/models/User');
 
 class UserService {
     static create(id, userName, name){
-        return  new User(id, userName, name, "Sin bio");ñ
+        return  new User(id, userName, name, "Sin bio");
+    }
+
+    static getInfo(user){
+        return [user.id, user.userName, user.name, user.bio];
     }
 }
 
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -9,4 +9,16 @@ describe("Tests for UserService", () => {
         expect(user.id).toBe(1);
         expect(user.bio).not.toBeUndefined();
     });
+   
+    test('2. Get all user data in a list', () => {
+        const user = UserService.create(1, "YoelVann", "Joel");
+        const userInfoInList = UserService.getInfo(user);
+
+        expect(userInfoInList[0]).toBe(1);
+        expect(userInfoInList[1]).toBe("YoelVann");
+        expect(userInfoInList[2]).toBe("Joel");
+        expect(userInfoInList[3]).toBe("Sin bio");
+    });
+
+
 });
