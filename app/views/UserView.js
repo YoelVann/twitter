60d095e3944f37053136b1d0dd35d36fd0e35399
Joel Hoil <yoelvann@gmail.com>
2022-04-25 00:58:18 -0500

    sprint 3 - req. 2 implemented

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -6,7 +6,11 @@ class UserView {
        
 
        if(payload === null ) {
-           return {error: "payload no existe"};
+           return {error: "payload no existegit"};
+       }
+
+       if(payload.id === null || payload.name === null || payload.userName === null){
+           return { error: "necesitan tener un valor válido" }
        }
 
        return new User(payload.id, payload.userName, payload.name);
--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -9,4 +9,12 @@ describe("Test for UserView class", () => {
         
         expect(result.error).toMatch(/payload no existe/);
     });
+    
+    test('Return an error object when try to create a new user with a payload missing properties', () => {
+        
+        const payload = {userName: null, name: 12, id: "id"};
+        const result = UserView.createUser(payload);
+
+        expect(result.error).toMatch(/necesitan tener un valor válido/);
+    });
 });
